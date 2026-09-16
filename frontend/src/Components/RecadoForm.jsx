47821047b11e00fdc27d919cb41b7e743bdb0e3f
Ricardo Silva Godoi <godoiricardo759@gmail.com>
2026-09-16 10:13:58 -0300

    feat: implementa formulário de recado

--- frontend/src/Components/RecadoForm.jsx
+++ frontend/src/Components/RecadoForm.jsx
@@ -1,3 +1,4 @@
+
 import {useState} from 'react';
 
 function RecadoForm({onAddRecado}) {
@@ -6,36 +7,44 @@ function RecadoForm({onAddRecado}) {
 
   function handleSubmit(event) {
     event.preventDefault();
-     onAddRecado({
-        titulo,
-        texto
-     });
-     setTitulo('');
-     setTexto('');
-     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="titulo">Título:</label>
-                <input
-                    type="text"
-                    id="titulo"
-                    value={titulo}
-                    onChange={(e) => setTitulo(e.target.value)}
-                />
-            </div>
-            <br />
-            <div>
-                <label htmlFor="texto">Texto:</label>
-                <textarea
-                    id="texto"
-                    value={texto}
-                    onChange={(e) => setTexto(e.target.value)}
-                />
-            </div>
-            <br />
-            <button type="submit">Adicionar Recado</button>
-        </form>
-     )
+
+    onAddRecado({
+      titulo,
+      texto
+    });
+
+    setTitulo('');
+    setTexto('');
   }
+   return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="titulo">Título:</label>
+        <input
+          type="text"
+          id="titulo"
+          value={titulo}
+          onChange={(e) => setTitulo(e.target.value)}
+        />
+      </div>
+
+      <br />
+
+      <div>
+        <label htmlFor="texto">Texto:</label>
+        <textarea
+          id="texto"
+          value={texto}
+          onChange={(e) => setTexto(e.target.value)}
+        />
+      </div>
+
+      <br />
+
+      <button type="submit">Adicionar Recado</button>
+    </form>
+  );
 }
+
 export default RecadoForm;
+
