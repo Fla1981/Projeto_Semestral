43dbce3b3f94883dcdbfa54e0fa15b99d89c9753
LuisFelipe2l <LUIS325rte@gmail.com>
2026-09-21 14:37:52 -0300

    feat: adiciona token de autenticação nas requisições

--- frontend/src/Servicos/Api.js
+++ frontend/src/Servicos/Api.js
@@ -1,19 +1,17 @@
-import Axios from "axios";
+import axios from "axios";
 
-const Api = Axios.create({
-    baseURL: "http://127.0.0.1:8000/api"
+const Api = axios.create({
+    baseURL: "http://localhost:8000/api"
 });
-// Adiciona um interceptor para incluir o token de autenticação em todas as requisições
+
 Api.interceptors.request.use((config) => {
-    // Recupera o token de autenticação do armazenamento local (localStorage)
     const token = localStorage.getItem("token");
+
     if (token) {
-        //envia o token de autenticação no cabeçalho Authorization da requisição
-        //identicar o usuário que está fazendo a requisição
         config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
-    
 });
 
 export default Api;
